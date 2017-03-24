0ec0412204d55049fddb31ed1eea1372262e2016
tos340 <thom@developer>
2017-03-24 17:53:24 +0100

    add inverse fourier

--- Assignment3/Oosterhuis_exercise3_2.m
+++ Assignment3/Oosterhuis_exercise3_2.m
@@ -4,8 +4,10 @@ function [ y ] = Oosterhuis_exercise3_2( normal_or_inverse,vector_x )
 %2^n and a variable which indicates whether the normal or inverse
 %transform needs to be calculated
 %output is vector of same size as input vector
+
+if normal_or_inverse == 0
 N = length(vector_x)
-omega = exp((-2*pi*1i)/N)
+%omega = exp((-2*pi*1i)/N)
 %recurtion:
 if N==1
     %b = -1i*(2 * pi * k * (n-1))/N
@@ -24,9 +26,28 @@ else
     y = [ y_odd + omega .* y_even; y_odd - omega .* y_even ]
     %omega = exp((-2*pi*1i)/N)
     %omega_vector = omega * ones(N,1)
-    %y = [Oosterhuis_exercise3_2(normal_or_inverse,vector_x_even) + omega_vector * Oosterhuis_exercise3_2(normal_or_inverse,vector_x_odd);Oosterhuis_exercise3_2(normal_or_inverse,vector_x_even) - omega_vector * Oosterhuis_exercise3_2(normal_or_inverse,vector_x_odd)] 
-    
-       
+    %y = [Oosterhuis_exercise3_2(normal_or_inverse,vector_x_even) + omega_vector * Oosterhuis_exercise3_2(normal_or_inverse,vector_x_odd);Oosterhuis_exercise3_2(normal_or_inverse,vector_x_even) - omega_vector * Oosterhuis_exercise3_2(normal_or_inverse,vector_x_odd)]  
+end
+%
+%LET's try the inverse fourier transform:
+elseif normal_or_inverse == 1
+    N=length(vector_x)
+    if N==1
+        y = vector_x
+    else
+        vector_x_even = vector_x(2:2:N)
+        vector_x_odd = vector_x(1:2:N)
+        N = N / 2
+        y_even = Oosterhuis_exercise3_2(normal_or_inverse,vector_x_even)
+        y_odd = Oosterhuis_exercise3_2(normal_or_inverse,vector_x_odd)
+        omega = exp(2 * pi * 1i / N) .^ (0:N-1)' 
+        %z = d .* y_bottom;
+        %y = [ y_top + z , y_top - z ];
+        y = [ y_odd + omega .* y_even; y_odd - omega .* y_even ]
+        %omega = exp((-2*pi*1i)/N)
     end
+end
+
+
 end
 
